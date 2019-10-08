cae0322f1012a9d6b95966902da9a9c857be85eb
Sergey Ilyushin <sergey.ilyushin@cgtribe.com>
2019-10-08 16:04:58 +0400

    Init

---
+++ GiphyApp/GiphyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GiphyApp
+//
+//  Created by Sergey Ilyushin on 08/10/2019.
+//  Copyright © 2019 Sergey Ilyushin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ GiphySDK/GiphySDK/GiphySDK.h
@@ -0,0 +1,19 @@
+//
+//  GiphySDK.h
+//  GiphySDK
+//
+//  Created by Sergey Ilyushin on 08/10/2019.
+//  Copyright © 2019 Sergey Ilyushin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GiphySDK.
+FOUNDATION_EXPORT double GiphySDKVersionNumber;
+
+//! Project version string for GiphySDK.
+FOUNDATION_EXPORT const unsigned char GiphySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GiphySDK/PublicHeader.h>
+
+
